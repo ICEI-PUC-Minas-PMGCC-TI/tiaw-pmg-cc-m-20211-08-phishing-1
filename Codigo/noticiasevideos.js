9f290da3a893b03adc1297a1657edb5266b3fd17
Luca Salazar <43450396+lucasalazar@users.noreply.github.com>
2021-06-03 21:11:29 -0300

    Atualização de arquivos
    Funcionalidades integradas e organizadas.

---
+++ Codigo/noticiasevideos.js
@@ -0,0 +1,6 @@
+noticia1 = window.open("https://inforchannel.com.br/2021/05/17/avast-detecta-dezenas-de-sms-de-phishing-relacionados-a-entrega-de-encomendas/");
+noticia2 = window.open("https://inforchannel.com.br/2020/08/27/quase-50-dos-ataques-de-phishing-ocorrem-em-dispositivos-moveis/");
+noticia3 = window.open("https://www.techtudo.com.br/noticias/2021/05/golpe-de-phishing-usa-vacina-contra-covid-para-instalar-trojan-bancario.ghtml");
+noticia4 = window.open("https://tiinside.com.br/14/05/2021/avast-detecta-diariamente-dezenas-de-sms-de-phishing-relacionados-a-entrega-de-encomendas/");
+noticia5 = window.open("https://www.terra.com.br/noticias/dino/phishing-se-torna-um-dos-golpes-digitais-mais-comuns-no-brasil,40a7de40edaa129edd8156744b1952c90z1jzaxy.html");
+noticia6 = window.open("https://www.contabeis.com.br/artigos/6625/golpes-virtuais-phishing-o-que-voce-precisa-saber-para-nao-se-tornar-a-proxima-vitima/");
